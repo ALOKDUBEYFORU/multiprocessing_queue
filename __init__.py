5b766f560742bc65663dde70a0dcbc7264a458a3
ALOKDUBEYFORU <DubeyAlok@rediffmail.com>
2024-10-11 06:42:50 +0530

    completed the main function

--- __init__.py
+++ __init__.py
@@ -7,5 +7,8 @@ if __name__ == "__main__":
     numbers = [2,3,5]
     q = multiprocessing.Queue()
     process1 = multiprocessing.Process(target=calc_square,args = (numbers,q))
-    pass
+    process1.start()
+    process1.join()
+    while q.empty() is False:
+        print(q.get())
 
