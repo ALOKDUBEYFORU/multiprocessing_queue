86408d0de39765d0ea48791e919826ba3801dd42
ALOKDUBEYFORU <DubeyAlok@rediffmail.com>
2024-10-11 05:36:03 +0530

    Done the changes to __init__.py and test.txt

--- __init__.py
+++ __init__.py
@@ -6,4 +6,6 @@ def calc_square(numbers,q):
 if __name__ == "__main__":
     numbers = [2,3,5]
     q = multiprocessing.Queue()
+    process1 = multiprocessing.Process(target=calc_square,args = (numbers,q))
     pass
+
